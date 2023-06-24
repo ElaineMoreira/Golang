4d572af56ed50ac564141f5ffd214f33321475e6
Elaine Moreira <elainefelipemoreira@gmail.com>
2023-06-24 15:53:37 -0300

    descomenta

--- introducao/main.go
+++ introducao/main.go
@@ -1,16 +1,17 @@
 // criar cabeçalho com package, ponto de partida para pacote
 
-// package main
+package main
 // criando função que chama a main
-// func main() {
-// 	print("hello word, mais uma linguagem pra conta!")
-// }
+func main() {
+	print("hello word, mais uma linguagem pra conta!")
+}
 // para ver na tela digite "go run nomeDoArquivo"
 // -------------------------------------------- //
 
 // package main
 
-// func main()  {
-// 	println("hello word convencional!")
-// }
+func main()  {
+println("hello word convencional!")
+}
 // Para executar o "main" é so digitar "./main" no terminal
+// -------------------------------------------- //
