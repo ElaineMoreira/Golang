4f4dc4757ce92702264b2ee77b0e7943f75d0c6b
Elaine Moreira <elainefelipemoreira@gmail.com>
2023-06-24 11:31:57 -0300

    hello word em go

---
+++ introducao/main.go
@@ -0,0 +1,8 @@
+// criar cabeçalho com package, ponto de partida para pacote
+
+package main
+// criando função que chama a main
+func main() {
+	print("hello word, mais uma linguagem pra conta!")
+}
+// para ver na tela digite "go run nomeDoArquivo"
