df6d136fea7781a4005217b88a60f72b4560142b
Elaine Moreira <elainefelipemoreira@gmail.com>
2023-06-24 16:03:06 -0300

    declarando variaveis

---
+++ declaraVariavel/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main()  {
+	name:= "Elaine" // dois ponto eh para declarar a primeira vez a variavel
+	name = "Elaine" // depois de declarado pode usar o =
+	var name string // outra forma de delarar variavel ao inves de :=
+}	
