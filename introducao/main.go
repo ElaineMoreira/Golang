f79e7f8034bc6446afef556f11c90dbffaaaee95
Elaine Moreira <elainefelipemoreira@gmail.com>
2023-06-24 15:42:05 -0300

    usando println

--- introducao/main.go
+++ introducao/main.go
@@ -1,8 +1,15 @@
 // criar cabeçalho com package, ponto de partida para pacote
 
-package main
+// package main
 // criando função que chama a main
-func main() {
-	print("hello word, mais uma linguagem pra conta!")
-}
+// func main() {
+// 	print("hello word, mais uma linguagem pra conta!")
+// }
 // para ver na tela digite "go run nomeDoArquivo"
+// -------------------------------------------- //
+
+package main
+
+func main()  {
+	println("hello word convencional!")
+}
